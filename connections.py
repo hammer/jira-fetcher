38f90df91133261261344d990ea104ed4e2258e5
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2010-12-26 17:36:18 -0800

    Build some simple reports from the data.

---
+++ connections.py
@@ -0,0 +1,74 @@
+import ConfigParser
+import os
+import psycopg2
+from suds.client import Client as suds_client
+
+class JiraConnection(object):
+  def __init__(self, url, username, password):
+    self._url = url
+    self._username = username
+    self._password = password
+    self._client = None
+    self._auth = None
+
+  @staticmethod
+  def make_connection_from_config(name):
+    config = ConfigParser.ConfigParser()
+    config.readfp(open(os.path.expanduser('~/.jira_fetcher')))
+
+    return JiraConnection(config.get(name, 'soap_endpoint'),
+                          config.get(name, 'username'),
+                          config.get(name, 'password'))
+
+  @property
+  def client(self):
+    if not self._client:
+      self._client = suds_client(self._url)
+    return self._client
+
+  @property
+  def auth(self):
+    if not self._auth:
+      self._auth = self.client.service.login(self._username, self._password)
+    return self._auth
+
+  def get_issues_from_filter(self, filter):
+    return self.client.service.getIssuesFromFilterWithLimit(self.auth, filter, 0, 100000)
+
+  def get_issues_from_jql(self, query):
+    return self.client.service.getIssuesFromJqlSearch(self.auth, query, 1000000)
+
+  def get_user(self, user):
+    return self.client.service.getUser(self.auth, user)
+
+  def close(self):
+    self.client.service.logout(self.auth)
+
+
+class PostgreSQLConnection(object):
+  def __init__(self, db, username):
+    self._db = db
+    self._username = username
+    self._conn = None
+
+  @property
+  def conn(self):
+    if not self._conn:
+      self._conn = psycopg2.connect("dbname=jira_fetcher user=hammer")
+    return self._conn
+  
+  def execute_sql(self, sql, params=None):
+    cur = self.conn.cursor()
+    cur.execute(sql, params)
+    self.conn.commit()
+    cur.close()
+
+  def fetch_sql(self, sql):
+    cur = self.conn.cursor()
+    cur.execute(sql)
+    results = cur.fetchall()
+    cur.close()
+    return results
+
+  def close(self):
+    self.conn.close()
---
+++ dashboard.py
@@ -0,0 +1,50 @@
+import os
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+
+from connections import PostgreSQLConnection
+
+# single row
+def get_unique_contributors():
+  db_conn = PostgreSQLConnection('jira_fetcher', 'hammer')
+  sql = 'SELECT COUNT(assignee), COUNT(DISTINCT assignee) FROM fixed_issues;'
+  results = db_conn.fetch_sql(sql)
+  db_conn.close()
+  return results[0]
+
+# multiple rows
+def get_unique_contributors_per_project():
+  db_conn = PostgreSQLConnection('jira_fetcher', 'hammer')
+  sql = 'SELECT project, COUNT(assignee) total_issues, COUNT(DISTINCT assignee) unique_contributors FROM fixed_issues GROUP BY project;'
+  results = db_conn.fetch_sql(sql)
+  db_conn.close()
+  return results
+
+class Application(tornado.web.Application):
+  def __init__(self):
+    handlers = [
+      (r"/", MainHandler),
+    ]
+    settings = dict(
+      template_path=os.path.join(os.path.dirname(__file__), "templates"),
+      static_path=os.path.join(os.path.dirname(__file__), "static"),
+    )
+    tornado.web.Application.__init__(self, handlers, **settings)
+
+class MainHandler(tornado.web.RequestHandler):
+  def get(self):
+    unique_contributors = get_unique_contributors()
+    unique_contributors_per_project = get_unique_contributors_per_project()
+    self.render("index.html",
+                unique_contributors=unique_contributors,
+                unique_contributors_per_project=unique_contributors_per_project)
+
+def main():
+  http_server = tornado.httpserver.HTTPServer(Application())
+  http_server.listen(8888)
+  tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+  main()
+
